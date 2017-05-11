8fcf22a81b9c768f9a6c9d0e235ada03a61ae12a
Gerlin Orlando Torres Saavedra <gtorress@everis.com>
2017-05-11 15:46:17 -0500

    Quartz programatico con web.xml

--- src/main/java/com/testing/quartz/listener/QuartzUnoListaner.java
+++ src/main/java/com/testing/quartz/listener/QuartzUnoListaner.java
@@ -1,51 +1,49 @@
 package com.testing.quartz.listener;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
-import javax.servlet.annotation.WebListener;
+import javax.servlet.ServletContextListener;
 
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.quartz.SimpleScheduleBuilder;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
-import org.quartz.ee.servlet.QuartzInitializerListener;
 import org.quartz.impl.StdSchedulerFactory;
 
-@WebListener
-public class QuartzUnoListaner extends QuartzInitializerListener {
+import com.testing.quartz.job.HelloJob;
+
+public class QuartzUnoListaner implements ServletContextListener {
+	Scheduler scheduler;
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		System.out.println("Context Destroyed");
+		try {
+			scheduler.shutdown();
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		super.contextInitialized(sce);
-		ServletContext ctx = sce.getServletContext();
-		StdSchedulerFactory factory = (StdSchedulerFactory) ctx.getAttribute(QUARTZ_FACTORY_KEY);
 		try {
-			Scheduler scheduler = factory.getScheduler();
+			scheduler = new StdSchedulerFactory().getScheduler();
 			// define the job and tie it to our HelloJob class
-			JobDetail job = JobBuilder.newJob(com.testing.quartz.job.HelloJob.class)
-					.withIdentity("dummyJobName", "grupo1").build();
+			JobDetail job = JobBuilder.newJob(HelloJob.class).withIdentity("CronQuartzJob", "Group").build();
 
 			// Trigger the job to run on the next round minute
 			Trigger trigger = TriggerBuilder.newTrigger().withIdentity("dummyTriggerName", "group1")
 					.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(5).repeatForever())
 					.build();
-			/*
-			 * Trigger trigger = TriggerBuilder .newTrigger()
-			 * .withIdentity("dummyTriggerName", "group1") .withSchedule(
-			 * CronScheduleBuilder.cronSchedule("0/5 * * * * ?")) .build();
-			 */
-			// schedule it
+
 			Scheduler schedulerAux = new StdSchedulerFactory().getScheduler();
 			schedulerAux.start();
 			schedulerAux.scheduleJob(job, trigger);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-	}
-
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-		super.contextDestroyed(sce);
 	}
 }
